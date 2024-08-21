62db795190d08c01daf10685d25afbd71ea861d3
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-08-21 14:43:47 +0300

    Upload project

---
+++ src/main/java/com/itchool/challenge2session31/Challenge2session31Application.java
@@ -0,0 +1,13 @@
+package com.itchool.challenge2session31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge2session31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge2session31Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itchool/challenge2session31/Challenge2session31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itchool.challenge2session31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge2session31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
